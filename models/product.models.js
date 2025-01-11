088b87390a22680b02bbe5ede68b19eb0d26f494
Rishi Gandhi <rishigandhi021@gmail.com>
2025-01-11 15:32:37 +0530

    HSN not unique

--- models/product.models.js
+++ models/product.models.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const productschema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   hsn_code: {
     type: Number,
     required: true,
-    unique: true
   },
   quantity: {
     type: Number,
